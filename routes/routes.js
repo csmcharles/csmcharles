2256cf92d473334051bcb1d29af25b2a9acf3c46
Caleb Charles <csmcharles@live.com>
2018-01-05 02:12:18 +1100

    Cleans up initial commit

--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function (req, res, next) {
-    res.render('index');
+    let locals = {
+        body: 'home',
+        title: 'csmcharles'
+    };
+    res.render('index', locals);
 });
 
 module.exports = router;
